5f5c212b552b0c95cda47e53c7b11cb99aec66fa
laliberte <laliberte.frederic@gmail.com>
2016-08-30 17:34:45 -0400

    Minor tweak to dependencies

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         install_requires = [
                             'requests>=1.1.0',
                             'requests_cache',
-                            'netcdf4',
+                            'netCDF4',
                             'pydap',
                             'mechanize'],
         zip_safe=False,
