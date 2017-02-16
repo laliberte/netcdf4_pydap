950dfd46dac7c2b6e0173b77f19969c55d631948
Frederic Laliberte <laliberte.frederic@gmail.com>
2017-02-16 08:08:13 -0500

    Propagate ServerError

--- netcdf4_pydap/pydap_fork/__init__.py
+++ netcdf4_pydap/pydap_fork/__init__.py
@@ -1,5 +1,5 @@
 from .pydap.src.pydap.apis.netCDF4 import Dataset
 from .pydap.src.pydap.cas import esgf, get_cookies
-from .pydap.src.pydap.exception import ServerError
+from .pydap.src.pydap.exceptions import ServerError
 
 __all__ = [Dataset, esgf, get_cookies, ServerError]
